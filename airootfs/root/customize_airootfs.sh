259cadaaec237540068172a0e0b54b287f3f4728
someidiotwithacomputer <299600125+someidiotwithacomputer@users.noreply.github.com>
2026-08-01 12:16:14 -0400

    added security suite and configs

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -4,3 +4,6 @@ set -e -u
 useradd -m -p '' -s /bin/bash -G wheel,storage,power,network,video,audio,disk tinfoil-live
 
 sed -i 's/^# %wheel ALL=(ALL:ALL) NOPASSWD: ALL/%wheel ALL=(ALL:ALL) NOPASSWD: ALL/' /etc/sudoers
+
+systemctl enable no-swap.service
+systemctl enable nftables.service
