d866a77aa07af4e08104533c575f84d507f66b8a
someidiotwithacomputer <12345678+someidiotwithacomputer@users.noreply.github.com>
2026-08-01 07:36:38 -0400

    Initial commit: Tinfoil Hat Linux II archiso profile

---
+++ airootfs/root/customize_airootfs.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -e -u
+
+useradd -m -p '' -s /bin/bash -G wheel,storage,power,network,video,audio,disk tinfoil-live
+
+sed -i 's/^# %wheel ALL=(ALL:ALL) NOPASSWD: ALL/%wheel ALL=(ALL:ALL) NOPASSWD: ALL/' /etc/sudoers
