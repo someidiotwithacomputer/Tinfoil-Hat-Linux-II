669eecb928954dc8bfc633cbc539e1abdf7e57bd
someidiotwithacomputer <299600125+someidiotwithacomputer@users.noreply.github.com>
2026-08-01 19:57:21 -0400

    Added custom fastfetch

--- profiledef.sh
+++ profiledef.sh
@@ -22,4 +22,6 @@ file_permissions=(
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
+  ["/usr/share/thl2/ascii/tux.txt"]="0:0:644"
+  ["/etc/fastfetch/config.jsonc"]="0:0:644"
 )
